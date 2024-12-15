a2773a4188a87fee15a0d25aba4e8a28349523a0
Schalk Neethling <schalk@mechanical.ink>
2024-12-15 02:43:01 +0200

    Linting and formatters. Also added a custom logger using chalk

--- script/update-blog-posts.js
+++ script/update-blog-posts.js
@@ -1,61 +1,71 @@
-import {readFileSync, writeFileSync} from 'node:fs';
+import { readFileSync, writeFileSync } from "node:fs";
 
-import {XMLParser} from "fast-xml-parser";
+import { XMLParser } from "fast-xml-parser";
+import { logger } from "../utils/logger";
 
 function getReadmeContent() {
-    return readFileSync('README.md', 'utf-8');
+  return readFileSync("README.md", "utf-8");
 }
 
 function updateReadmeContent(content) {
-    writeFileSync('README.md', content, 'utf-8');
+  writeFileSync("README.md", content, "utf-8");
 }
 
 function postsToMarkdown(posts) {
-    const options = {
-        day: 'numeric',
-        month: 'long',
-        timeZone: 'UTC',
-        weekday: 'long',
-        year: 'numeric',
-      };
+  const options = {
+    day: "numeric",
+    month: "long",
+    timeZone: "UTC",
+    weekday: "long",
+    year: "numeric",
+  };
 
-    return posts.map(post => {
-        const pubDate = new Date(post.pubDate);
-        const content = [`### [${post.title}](${post.link})`, post.description, pubDate.toLocaleDateString(undefined, options)].join('\n\n');
-        return content.trim();
-    }).join('\n\n');
+  return posts
+    .map((post) => {
+      const pubDate = new Date(post.pubDate);
+      const content = [
+        `### [${post.title}](${post.link})`,
+        post.description,
+        pubDate.toLocaleDateString(undefined, options),
+      ].join("\n\n");
+      return content.trim();
+    })
+    .join("\n\n");
 }
 
 const main = async () => {
-    const DRYRUN = Boolean(JSON.parse(process.env.DRYRUN ?? "false"));
+  const DRYRUN = Boolean(JSON.parse(process.env.DRYRUN ?? "false"));
 
-    try {
-        const response = await fetch('https://schalkneethling.com/rss.xml');
+  try {
+    const response = await fetch("https://schalkneethling.com/rss.xml");
 
-        if (response.ok) {
-            const data = await response.text();
-            const parser = new XMLParser();
-            const xml = parser.parse(data);
+    if (response.ok) {
+      const data = await response.text();
+      const parser = new XMLParser();
+      const xml = parser.parse(data);
 
-            const {rss} = xml;
-            const entries = rss.channel.item.slice(0, 5);
+      const { rss } = xml;
+      const entries = rss.channel.item.slice(0, 5);
 
-            const postsContainerRegExp = /<!-- blog posts -->[\s\S]*<!-- \/blog posts -->/;
-            const readmeContent = getReadmeContent();
+      const postsContainerRegExp =
+        /<!-- blog posts -->[\s\S]*<!-- \/blog posts -->/;
+      const readmeContent = getReadmeContent();
 
-            const postsAsMarkdown = postsToMarkdown(entries);
-            const updatedReadmeContent = readmeContent.replace(postsContainerRegExp, `<!-- blog posts -->\n${postsAsMarkdown}\n<!-- /blog posts -->`);
+      const postsAsMarkdown = postsToMarkdown(entries);
+      const updatedReadmeContent = readmeContent.replace(
+        postsContainerRegExp,
+        `<!-- blog posts -->\n${postsAsMarkdown}\n<!-- /blog posts -->`,
+      );
 
-
-            if (DRYRUN) {
-                console.log(updatedReadmeContent);
-            } else {
-                updateReadmeContent(updatedReadmeContent);
-            }
-        }
-    } catch (error) {
-        console.error(error);           
+      if (DRYRUN) {
+        logger.log(updatedReadmeContent);
+      } else {
+        updateReadmeContent(updatedReadmeContent);
+      }
     }
-}
+  } catch (error) {
+    logger.error(error);
+  }
+};
 
 main();
